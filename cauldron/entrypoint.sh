fd20aec70c1d765d3c0f2de826bb03a752599b87
JJMerchante <jj.merchante@gmail.com>
2019-03-25 12:21:19 +0100

    adding networks in containers for resolving some issues

--- cauldron/entrypoint.sh
+++ cauldron/entrypoint.sh
@@ -4,7 +4,7 @@
 cp /code/settings_secret.py /code/cauldron/Cauldron2/Cauldron2/settings_secret.py
 
 # Create a database for mordred
-mysql -u grimoirelab -h 127.0.0.1 -e "create database test_db;"
+mysql -u grimoirelab -h grimoirelab_service -e "create database test_db;"
 
 # Run cauldron server
 cd /code/cauldron/Cauldron2
@@ -12,7 +12,7 @@ echo "Apply migrations"
 python3 manage.py makemigrations
 python3 manage.py migrate
 cd /code/cauldron/Cauldron2/MordredManager
-echo "Run mordred manager that wait for new repositories for being analyzed"
+echo "Run mordred manager that waits for new repositories for being analyzed"
 python3 manager.py &
 cd ..
 echo "Start the cauldron server"
