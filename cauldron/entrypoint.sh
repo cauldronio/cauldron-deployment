34570488a953ad605e0881f4697cb6a9f90be213
JJMerchante <jj.merchante@gmail.com>
2019-04-02 15:04:07 +0200

    Fix: Now the cauldron server waits until a database is created.

--- cauldron/entrypoint.sh
+++ cauldron/entrypoint.sh
@@ -4,7 +4,11 @@
 cp /code/settings_secret.py /code/cauldron/Cauldron2/Cauldron2/settings_secret.py
 
 # Create a database for mordred
-mysql -u grimoirelab -h grimoirelab_service -e "create database test_sh;"
+until mysql -u grimoirelab -h grimoirelab_service -e "create database test_sh;"
+do
+  echo $(date +"%x %X") - Waiting for mysql
+  sleep 2
+done
 
 # Run cauldron server
 cd /code/cauldron/Cauldron2
