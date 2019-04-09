f6951aa8932a26436cb3d53a94a97a598ef3c176
JJMerchante <jj.merchante@gmail.com>
2019-04-09 18:09:30 +0200

    New deployment! Mordred in a container

--- cauldron/entrypoint.sh
+++ cauldron/entrypoint.sh
@@ -20,9 +20,6 @@ cd /code/cauldron/Cauldron2
 echo "Apply migrations"
 python3 manage.py makemigrations
 python3 manage.py migrate
-cd /code/cauldron/Cauldron2/MordredManager
-echo "Run mordred manager that waits for new repositories for being analyzed"
-python3 manager.py &
-cd ..
+
 echo "Start the cauldron server"
 python3 manage.py runserver 0.0.0.0:8000
