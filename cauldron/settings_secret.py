0fbbbf49b7d798a6976ee3df94d8c42083ff9216
JJMerchante <jj.merchante@gmail.com>
2019-05-06 18:29:00 +0200

    Adding gitlab support. Refactoring the playbooks

--- cauldron/settings_secret.py
+++ cauldron/settings_secret.py
@@ -9,6 +9,15 @@ DJANGO_KEY = ''
 GH_CLIENT_ID = ''
 GH_CLIENT_SECRET = ''
 
+# Create a GitLab Oauth Application and get the keys
+GL_CLIENT_ID = ''
+GL_CLIENT_SECRET = ''
+
+# Create a private Token for Gitlab.
+# This will not be necessary in the future.
+# Now perceval cannot doesn't allow oauth tokens
+GL_PRIVATE_TOKEN = ''
+
 # Database configuration
 DB_NAME = 'db_cauldron'
 DB_USER = 'grimoirelab'
