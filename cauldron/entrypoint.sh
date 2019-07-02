73664bbcd1275020e7a0873df016740feda7603f
JJMerchante <jj.merchante@gmail.com>
2019-07-02 13:55:49 +0200

    Go back to old Django structure
    Delete nginx, supervisor and gunicorn from Django container
    Expose port 8000 for Django in Dockerfile
    Certbot will not run anymore in this container

--- cauldron/entrypoint.sh
+++ cauldron/entrypoint.sh
@@ -4,16 +4,6 @@ echo "Apply migrations"
 python3 /code/cauldron/Cauldron2/manage.py makemigrations
 python3 /code/cauldron/Cauldron2/manage.py migrate
 
-rm /etc/nginx/sites-enabled/default
-supervisord
-supervisorctl reread
-supervisorctl update
-supervisorctl restart cauldron
-
-if [[ $CERTBOT_ENABLED -eq 1 ]]; then
-    certbot --nginx -n -d $CERTBOT_DOMAIN --agree-tos --email $CERTBOT_MAIL
-fi
-service nginx restart
-
-# Sleep forever
-tail -f /dev/null
+echo "Start the cauldron server"
+cd /code/cauldron/Cauldron2/
+python3 manage.py runserver 0.0.0.0:8000
