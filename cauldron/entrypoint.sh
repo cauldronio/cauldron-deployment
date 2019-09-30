16b12fe2bef24bfc262ebf0522758198d7d42d3c
Sergio Merino <sergiohdez94@gmail.com>
2019-09-30 07:32:14 +0000

    Modify some Dockerfile vars

--- cauldron/entrypoint.sh
+++ cauldron/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Apply migrations"
-python3 /code/cauldron/Cauldron2/manage.py makemigrations
-python3 /code/cauldron/Cauldron2/manage.py migrate
+python3 /code/cauldron-web/Cauldron2/manage.py makemigrations
+python3 /code/cauldron-web/Cauldron2/manage.py migrate
 
 echo "Start the cauldron server"
-cd /code/cauldron/Cauldron2/
+cd /code/cauldron-web/Cauldron2/
 python3 manage.py runserver 0.0.0.0:8000
