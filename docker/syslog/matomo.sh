68ff77e37e36878b38dc3978101a679955d57e1e
JJMerchante <jj.merchante@gmail.com>
2020-04-27 13:40:56 +0200

    [matomo] Use json format for nginx logs
    This commit updates the log's format for nginx. Now it is the json
    format defined by Matomo and fix the error that didn't show the
    browser type. Only access logs are formatted.
    
    Signed-off-by: JJMerchante <jj.merchante@gmail.com>

--- docker/syslog/matomo.sh
+++ docker/syslog/matomo.sh
@@ -3,4 +3,4 @@
 /log-analytics/import_logs.py \
  --url=$MATOMO_URL/ \
  --idsite=1 --recorders=1 --enable-http-errors --enable-http-redirects --enable-static --enable-bots \
- --login=$MATOMO_USER --password=$MATOMO_PASSWORD --log-format-name=common --recorder-max-payload-size=50 - &>> /log-analytics/matomo.log
+ --login=$MATOMO_USER --password=$MATOMO_PASSWORD --log-format-name=nginx_json --recorder-max-payload-size=50 - &>> /log-analytics/matomo.log
